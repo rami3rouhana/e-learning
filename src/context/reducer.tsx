5c2b18420022789f18419039395205c51841367b
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-08 23:48:57 +0300

    frontend: add reducer

---
+++ src/context/reducer.tsx
@@ -0,0 +1,52 @@
+import { ActionType, GlobalStateInterface, MovieType } from './types';
+import { initialState } from './index';
+
+const Reducer = (state: GlobalStateInterface, action: ActionType): any => {
+  switch (action.type) {
+    case 'GET_STUDENTS':
+      return {
+        ...state,
+        students: action.payload,
+      };
+    case 'ERROR':
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case 'GET_COURSES':
+      return {
+        ...state,
+        courses: action.payload,
+      };
+    case 'GET_INSTRUCTORS':
+      return {
+        ...state,
+        instructors: action.payload,
+      };
+    case 'GET_STUDENTS_COURSES':
+      return {
+        ...state,
+        classes: action.payload,
+      };
+    case 'GET_ASSIGNEMENTS':
+      return {
+        ...state,
+        assignements: action.payload,
+      };
+    case 'GET_ANNOUNCEMENTS':
+      return {
+        ...state,
+        anouncements: action.payload,
+      };
+    case 'ADD_COURSE' || 'ADD_INSTRUCTOR' || 'ADD_STUDENT' || 'CREATE_ASSIGNEMENT' || 'ADD_ASSIGNEMENT' || 'ADD_ANNOUNCEMENT' || 'ADD_STUDENTS_COURSE':
+      return {
+        ...state,
+      };
+    case 'PURGE_STATE':
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
