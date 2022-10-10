d3f1ffffc5984261aeaad13a977ab8abcd4cfafe
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-10 16:35:08 +0300

    backend: add student authentication

---
+++ e-learning-backend/app/Http/Middleware/StudentAuth.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class StudentAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $key = env('JWT_SECRET');
+
+        $headers = getallheaders();
+
+        $jwtToken =  explode(" ", $headers["Authorization"])[1];
+
+        $decoded = JWT::decode($jwtToken, new Key($key, 'HS256'));
+
+        if ($decoded->role == '3') {
+            return $next($request);
+        } else {
+            return response()->json([
+                'token' => 'invalid',
+            ]);
+        }
+    }
+}
