2ee096724faa79ad1bcebbf916798ac777fb5be3
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-11 11:55:22 +0300

    backend: refactor course controller

--- e-learning-backend/app/Http/Controllers/CourseController.php
+++ e-learning-backend/app/Http/Controllers/CourseController.php
@@ -5,20 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Course;
 use App\Models\User;
+use App\Models\StudentClass;
 
 class CourseController extends Controller
 {
     public function getCourses()
     {
-        $result = Course::get();
+        if ($this->me()['role'] === '2') {
 
-        if (Count($result)) {
-            return response()->json(["courses" => $result, "jwt" => $this->refresh(), "success" => true], 200);
+            $courses = Course::where('instructor_id', $this->me()['id'])->get();
+
+            if (Count($courses)) {
+                return response()->json(["assignements" => $courses, "jwt" => $this->refresh(), "success" => true], 200);
+            } else {
+                return response()->json(["Error" => "Something went wrong."], 400);
+            }
         } else {
-            return response()->json(["Error" => "Something went wrong."], 400);
+            $students = StudentClass::where('student_id', $this->me()['id'])->groupby('course_id')->get();
+            $result = [];
+            foreach ($students as $student) {
+                $result[] = Course::where('_id', $student->course_id)->get();
+            }
+            if (Count($result)) {
+                return response()->json(["assignements" => $result, "jwt" => $this->refresh(), "success" => true], 200);
+            } else {
+                return response()->json(["Error" => "Something went wrong."], 400);
+            }
         }
     }
-    
+
     public function addCourse(Request $request)
     {
         $courseExist = Course::where('course', $request->course)->first();
@@ -26,17 +41,16 @@ class CourseController extends Controller
         if ($courseExist) {
             return response()->json(["course" => "Already Taken"], 400);
         }
-        $instructorExist = User::where([['_id', $request->id], ['role', '2']])
-            ->get();
+        $instructorExist = User::where([['_id', $request->id], ['role', '2']])->first();
 
-        if (!count($instructorExist)) {
+        if ($instructorExist) {
             return response()->json(["instructor" => 'Not found'], 400);
         }
 
         $course = new Course;
 
         $course->course = $request->course;
-        $course->instructor_id = $request->id;
+        $course->instructor_id = $request->id || $this->me()['id'];
 
         $course->save();
 
