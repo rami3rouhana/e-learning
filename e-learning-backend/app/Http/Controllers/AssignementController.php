59ca2207df0ffd1eea852825c5b5691275aa552f
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-10-11 11:55:09 +0300

    backend: refactor assignemet controller

--- e-learning-backend/app/Http/Controllers/AssignementController.php
+++ e-learning-backend/app/Http/Controllers/AssignementController.php
@@ -4,17 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Course;
+use App\Models\StudentClass;
 use App\Models\Assignement;
 
 class AssignementController extends Controller
 {
-    public function getAssignements(Request $request)
+    public function getAssignements()
     {
-        $result = Assignement::where('course_id', $request->id)->get();
-        if (Count($result)) {
-            return response()->json(["assignements" => $result, "jwt" => $this->refresh(), "success" => true], 200);
+        if ($this->me()['role'] === '2') {
+            $courses = Course::where('instructor_id', $this->me()['id'])->get();
+            $result = [];
+            foreach ($courses as $course) {
+                $result = Assignement::where('course_id', $course->id)->get();
+            }
+            if (Count($result)) {
+                return response()->json(["assignements" => $result, "jwt" => $this->refresh(), "success" => true], 200);
+            } else {
+                return response()->json(["Error" => "Something went wrong."], 400);
+            }
         } else {
-            return response()->json(["Error" => "Something went wrong."], 400);
+            $students = StudentClass::where('student_id', $this->me()['id'])->groupby('course_id')->get();
+            $result = [];
+            foreach ($students as $student) {
+                $result[] = Assignement::where('course_id', $student->course_id)->get();
+            }
+            if (Count($result)) {
+                return response()->json(["assignements" => $result, "jwt" => $this->refresh(), "success" => true], 200);
+            } else {
+                return response()->json(["Error" => "Something went wrong."], 400);
+            }
         }
     }
 
@@ -22,9 +40,33 @@ class AssignementController extends Controller
     {
         $assignementExist = Assignement::where('assignement', $request->assignement)->first();
 
+        
+        if ($this->me()['role'] === '3') {
+            if (!$assignementExist){
+                return response()->json(["assignement" => "Doesn't Exist."], 400);
+            }
+            $assignement = StudentClass::where([['student_id', $this->me()['id']],['course_id',$assignementExist->course_id]])->first();
+            
+            if($assignement){
+                $assignement->assignement = $request->file;
+                $assignement->save();
+            } else {
+                $assignement = new StudentClass;
+                $assignement->assignement_id = $assignementExist->id;
+                $assignement->course_id = $assignementExist->course_id;
+                $assignement->student_id = $this->me()['id'];
+                $assignement->assignement = $request->file;
+                $assignement->save();
+            }
+            
+            return response()->json(["id" => $assignement->_id], 200);
+        }
+
+        
         if ($assignementExist) {
             return response()->json(["assignement" => "Already Taken"], 400);
         }
+
         $courseExist = Course::where('_id', $request->id)->get();
 
         if (!count($courseExist)) {
@@ -35,6 +77,7 @@ class AssignementController extends Controller
 
         $course->assignement = $request->assignement;
         $course->course_id = $request->id;
+        $course->assignement_id = $request->assignement_id;
 
         $course->save();
 
